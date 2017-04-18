98e136690f491e6e3d64cc2dc6f444e5fa544540
amanda lee anderson <amandalee.anderson@gmail.com>
2017-04-18 11:55:08 -0500

    some components, some more addons

---
+++ mirage/config.js
@@ -0,0 +1,44 @@
+export default function() {
+	this.namespace = '/api';
+
+	this.get('/rentals', function() {
+		return {
+			data: [{
+				type: 'rentals',
+				id: 'grand-old-mansion',
+				attributes: {
+					title: 'Testing 1',
+					owner: 'Veruca Salt',
+					city: 'San Francisco',
+					type: 'Estate',
+					bedrooms: 15,
+					image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+				}
+			}, 
+			{
+				type: 'rentals',
+				id: 'urban-living',
+				attributes: {
+					title: 'Testing 2',
+					owner: 'Mike Teavee',
+					city: 'Seattle',
+					type: 'Condo',
+					bedrooms: 1,
+					image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
+				}
+			}, 
+			{
+				type: 'rentals',
+				id: 'downtown-charm',
+				attributes: {
+					title: 'Testing 3',
+					owner: 'Violet Beauregarde',
+					city: 'Portland',
+					type: 'Apartment',
+					bedrooms: 3,
+					image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
+				}
+			}]
+		};
+	});
+}
